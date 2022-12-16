f169cc82db09d513f3f4a508efda9193760acc75
Jerica Huang <jerica.huang@gmail.com>
2022-12-16 11:51:24 -0800

    useEffect and APIs

--- src/App.js
+++ src/App.js
@@ -1,66 +1,99 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 import Navbar from "./components/Navbar";
 import BikeList from "./components/BikeList.js";
+import axios from "axios";
 
-const INITIAL_BIKES = [
-  {
-    id: 1,
-    name: "Cool Bike",
-    size: 50,
-    price: 100,
-    type: "special",
-  },
-  {
-    id: 2,
-    name: "BikeMatic 3000",
-    size: 40,
-    price: 20,
-    type: "duplicate",
-  },
-  {
-    id: 3,
-    name: "My new bike!!",
-    size: 80,
-    price: 100,
-    type: "unique",
-  },
-];
+// const INITIAL_BIKES = [
+//   {
+//     id: 1,
+//     name: "Cool Bike",
+//     size: 50,
+//     price: 100,
+//     type: "special",
+//   },
+//   {
+//     id: 2,
+//     name: "BikeMatic 3000",
+//     size: 40,
+//     price: 20,
+//     type: "duplicate",
+//   },
+//   {
+//     id: 3,
+//     name: "My new bike!!",
+//     size: 80,
+//     price: 100,
+//     type: "unique",
+//   },
+// ];
 
 function App() {
-  const initialCopy = INITIAL_BIKES.map((bike) => {
-    return { ...bike };
-  });
+  const [bikesList, setBikesList] = useState([]);
 
-  const [bikesList, setBikesList] = useState(initialCopy);
+  const URL = "http://localhost:5000/bike";
+  useEffect(()=>{
+    axios.get(URL)
+    .then((res) => {
+      //console.log(res);
+      const bikesAPIResCopy = res.data.map((bike) => {
+        return {
+          ...bike
+      }})
+      setBikesList(bikesAPIResCopy);
+        
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }, []);
+
+  // const initialCopy = INITIAL_BIKES.map((bike) => {
+  //   return { ...bike };
+  // });
+
 
   const updatePrice = (bikeId, updatedPrice) => {
     console.log("updatePrice called");
     const newBikesList = [];
-    for (const bike of bikesList) {
-      if (bike.id !== bikeId) {
-        newBikesList.push(bike);
-      } else {
-        const newBike = {
-          ...bike,
-          price: updatedPrice,
-        };
-        newBikesList.push(newBike);
+    axios.patch(`${URL}/${bikeId}/${updatedPrice}`)
+    .then((res)=>{
+      for (const bike of bikesList) {
+        if (bike.id !== bikeId) {
+          newBikesList.push(bike);
+        } else {
+          const newBike = {
+            ...bike,
+            price: updatedPrice,
+          };
+          newBikesList.push(newBike);
+        }
       }
-    }
-    setBikesList(newBikesList);
+      setBikesList(newBikesList);
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+    
   };
 
   const deleteBike = (bikeId) => {
     console.log("deleteBike Called");
-    const newBikesList = [];
-    for (const bike of bikesList) {
-      if (bike.id !== bikeId) {
-        newBikesList.push(bike);
+    axios.delete(`${URL}/${bikeId}`)
+    .then(()=>{
+      const newBikesList = [];
+      for (const bike of bikesList) {
+        if (bike.id !== bikeId) {
+          newBikesList.push(bike);
+        }
       }
-    }
-    setBikesList(newBikesList);
+      setBikesList(newBikesList);
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+    
   };
 
   return (
